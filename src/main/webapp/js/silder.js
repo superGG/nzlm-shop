4752b6e63f1b06f19018c4e056ff213a9112dd88
wjt <627937064@qq.com>
2015-09-09 19:49:51 +0800

    edwad

--- src/main/webapp/js/silder.js
+++ src/main/webapp/js/silder.js
@@ -12,42 +12,77 @@ $(function(){
 			index = 1;
 		}
 
+	$.fn.silder = function(options){
 
-		if(!((nowX === 0 || nowX ===5)&& index === -1) && !(nowX === 4 && index === 1)){
-			nowX = nowX + index;
-			oul.animate({'left':-nowX*100 + '%'},500);
-		}
+		var defaults = {
+			nowIndex : 0,
+			timeId : 0,
+			me : null,
+			flag: false
+		};
 
-		console.log(nowX);
-	});
 
-	function silder(){
-		if(nowX==4){
-			oul.find(':first').css({
-					'position': 'relative',
-					'left':'300%'
+		var options = $.extend(defaults,options);
+
+		var startShow = function(){
+
+			console.log(defaults.nowIndex);
+			defaults.nowIndex++;
+			var $this = $(this);
+
+			$this.animate({'left':-defaults.nowIndex*100 + '%'},1000,function(){
+
+				if(defaults.nowIndex === defaults.imgNum){
+
+					$this.css({
+						'left': '0'
+					});
+
+					defaults.nowIndex = 0;
+				}
 			});
 		}
+		
+		var me = this;
 
-		oul.animate({'left':-nowX*100 + '%'},1000,function(){
-			console.log(nowX);
-			if(nowX>4){
-				oul.find(':first').css({
-					'position': 'static',
-				});
-				oul.css('left',0);
-				nowX = 0;
+		defaults.timeId = setInterval(function(){
+			startShow.call(me.find('ul'));
+		},3000);
+
+		me.find('ol li').each(function(i){
+			$(this).bind('click',function(){
+
+				var myUl = me.find('ul');
+
+				if(i === 0 && defaults.nowIndex !== 0){
+
+					defaults.nowIndex--;
+					myUl.animate({'left':-defaults.nowIndex*100 + '%'});
+				}else if(i === 1 ){
+
+					console.log(defaults.nowIndex);
+					defaults.nowIndex++;
+
+					if(defaults.nowIndex === defaults.imgNum){
+						defaults.nowIndex = 0;
+					}
+					myUl.stop().animate({'left':-defaults.nowIndex*100 + '%'});
+				}
+			})
+		}); 
+
+		me.find('ol li').bind({
+			'mouseover':function(){
+				clearInterval(defaults.timeId);
+			},
+			'mouseleave':function(){
+				defaults.timeId = setInterval(function(){
+					startShow.call(me.find('ul'));
+				},3000);
 			}
-		});
+		})
+	}
 
-		nowX++;
+})(jQuery);
 
-	};
-
-	timeId = setInterval(silder,3000);
-
-	// $('#wrap').bind({
-	// 	'mouseover':function(){clearInterval(timeId)},
-	// 	'mouseout':function(){timeId = setInterval(silder,1500)}
-	// });
-});
+$('#wrap').silder({imgNum:4});
