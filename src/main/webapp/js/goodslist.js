88bb5bef1b7e51cc2ae0814bf3a7da6658154c88
LaLa <798555920@qq.com>
2015-09-28 15:12:31 +0800

    修改url

--- src/main/webapp/js/goodslist.js
+++ src/main/webapp/js/goodslist.js
@@ -9,10 +9,12 @@ $(function () {
     console.log(attributename);
     console.log(attributevalue);
 
+    //下面是初始化的商品数据
     var goodslist_htmlobj = $.ajax({url: "queryKeyWord.txt", async: false});
+//    var goodslist_htmlobj = $.ajax({url: "localhost:8080/shoppingsolr2/solr_queryByType.action?"+tmptype, async: false});
 
     var attributes_htmlobj = $.ajax({url: "getAttribute.txt", async: false});
-//    var attributes_htmlobj = $.ajax({url: "http://localhost:8080/shoppingsolr2/goods_getAttrbutes.action?"+tmptype, async: false});
+//    var attributes_htmlobj = $.ajax({url: "http://localhost:8080/shoppingsolr2/solr_getAttrbutes.action?"+tmptype, async: false});
 
     var attributes = eval('(' + attributes_htmlobj.responseText + ")");
 
@@ -21,43 +23,6 @@ $(function () {
     var postModel = [];
 
     postModel.push({ name : attributename, value : attributevalue});
-
-//    $.each(categories, function (i, data) {
-//
-//        $.each(data.type2, function (i, data2) {
-//
-//            $(".main .container .class_selection_container .class_selection ul").append(
-//                $("<li></li>").append(
-//                    $("<a></a>").attr("href", "javascript:;").html(data2.name).on("click", function () {
-//                        console.log(data2.name);
-//                    }
-//                    )
-//                )
-//            );
-//
-//            $.each(data2.type3, function (i, data3) {
-//
-//                $(".main .container .class_selection_container .class_selection ol").append(
-//                    $("<li></li>").append(
-//                        $("<a></a>").attr("href", "javascript:;").html(data3)).on("click", function () {
-//                            console.log(data3);
-//                            goodslist_htmlobj = $.ajax({url: "queryKeyWord2.txt", async: false});
-//                            keyWordGoods = eval(goodslist_htmlobj.responseText);
-//                            $(".main .container .goods_list .goods_show ul").empty();
-//                            //console.log(keyWordGoods);
-//                            $.each(keyWordGoods, function (i, data) {
-//                                var goods = $("<li></li>").append(
-//                                    $("<div class='good_img'></div>").append(
-//                                        $("<img />").attr("src", "img/Q5423EFZ41_CGR0_N01.JPG"))).append(
-//                                    $("<a class='name'></a>").attr("href", data.goodspic).html(data.goodsname)).append(
-//                                    $("<span class='price'></span>").text(data.goodsprice)).append(
-//                                    $("<em></em>").append($("<img />").attr("src", "img/icon_cart.png/")).html("1000"));
-//                                $(".goods_show > ul").append(goods);
-//                            });
-//                        }));
-//            });
-//        });
-//    });
 
     $.each(attributes, function (keyname, data) {
         var myul = $("<ul></ul>");
@@ -98,7 +63,6 @@ $(function () {
             $("<div></div>").attr("class", "classification_attr").append(
                 $("<div></div>").attr("class", "attrname").html(keyname)).append(
                 myul));
-        //$(".classification")
 
     });
 
