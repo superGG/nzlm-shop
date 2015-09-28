70eb0e55b92b05b8d378fb09a7bc03c7965c7d5d
LaLa <798555920@qq.com>
2015-09-28 15:02:19 +0800

    关联数据库
    d

--- src/main/java/com/earl/shopping/action/GoodsAction.java
+++ src/main/java/com/earl/shopping/action/GoodsAction.java
@@ -1,17 +1,14 @@
 package com.earl.shopping.action;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.InvocationTargetException;
 
+import org.apache.commons.beanutils.BeanUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.solr.client.solrj.SolrServerException;
 
 import com.earl.solrj.SolrService;
+import com.earl.solrj.query.pojo.GoodsPo;
 import com.earl.solrj.query.pojo.GoodsVo;
-import com.earl.util.ToJson;
 
 /**
  * 
@@ -20,7 +17,7 @@ import com.earl.util.ToJson;
  * @author Administrator
  * 
  */
-public class GoodsAction extends BaseAction<GoodsVo> {
+public class GoodsAction extends BaseAction<GoodsPo> {
 
 	Logger logger = LogManager.getLogger(this.getClass());
 	/**
@@ -30,59 +27,15 @@ public class GoodsAction extends BaseAction<GoodsVo> {
 
 	protected InputStream jsonInputStream;
 
-	public InputStream getJsonInputStream() {
-		return jsonInputStream;
-	}
-
-	private SolrService solrUtil = new SolrService();
-
-	/**
-	 * 查询商品的最低价格.
-	 */
-	private float minPrice;
-
-	/**
-	 * 查询商品的最高价格.
-	 */
-	private float maxPrice;
+	private SolrService solrServer = new SolrService();
 
 	/**
 	 * 搜索的关键字,直接映射到goodslable,goodsname.
 	 */
 	private String keyWord;
 
-	/**
-	 * 查询的最低价格
-	 * 
-	 * @return the minPrice
-	 */
-	public final float getMinPrice() {
-		return minPrice;
-	}
-
-	/**
-	 * 查询的最高价格
-	 * 
-	 * @param minPrice
-	 *            the minPrice to set
-	 */
-	public final void setMinPrice(float minPrice) {
-		this.minPrice = minPrice;
-	}
-
-	/**
-	 * @return the maxPrice
-	 */
-	public final float getMaxPrice() {
-		return maxPrice;
-	}
-
-	/**
-	 * @param maxPrice
-	 *            the maxPrice to set
-	 */
-	public final void setMaxPrice(float maxPrice) {
-		this.maxPrice = maxPrice;
+	public InputStream getJsonInputStream() {
+		return jsonInputStream;
 	}
 
 	public String getKeyWord() {
@@ -96,183 +49,37 @@ public class GoodsAction extends BaseAction<GoodsVo> {
 	// 添加索引.
 	public void addGoodsAction() throws Exception {
 		// 从前端获取到的Po 转成 Vo 再进行,同时保存po到数据库
-		solrUtil.addBeanIndex(model);
+		goodsServer.save(model);
+		GoodsVo goodsVo = new GoodsVo();
+		BeanUtils.copyProperties(goodsVo, model);
+		solrServer.addBeanIndex(goodsVo);
 	}
 
-	// 通过id删除商品.
+	/**
+	 * 通过id删除商品.
+	 * 
+	 * @throws Exception
+	 */
 	public void deleGoodsAction() throws Exception {
-
-		solrUtil.deleteById(model);
+		
+		goodsServer.deleteById(model.getId());
+		GoodsVo goodsVo = new GoodsVo();
+		BeanUtils.copyProperties(goodsVo, model);
+		solrServer.deleteById(goodsVo);
 	}
-
-	/**
-	 * 通过id删除商品
-	 * 
-	 * @throws Exception
-	 */
-	public void deleteByIdAction() throws Exception {
-		solrUtil.deleteById(model);
-	}
-
-	/**
-	 * 
-	 * 通过查询条件删除索引.
-	 * 
-	 * @throws Exception
-	 * 
-	 */
-	// public void deleteByQuery() {
-	// solrService.deleteByQuery(query);
-	// }
-
-	/**
-	 * 查询价格区间的商品.
-	 * 
-	 * @throws Exception
-	 */
-	public String queryByPrice() throws Exception {
-		if (minPrice != maxPrice && maxPrice > minPrice) {
-			logger.debug("minPrice : "+minPrice);
-			logger.debug("maxPrice : "+maxPrice);
-			String jsonString = ToJson.getGson().toJson(
-					solrUtil.queryBeans(model, minPrice, maxPrice));
-			logger.debug("jsonString : "+jsonString);
-			jsonInputStream = new ByteArrayInputStream(
-					jsonString.getBytes("utf-8"));
-		}
-		return "done";
-	}
-
-	/**
-	 * @author 宋文光.
-	 * @return String
-	 * @throws Exception
-	 */
-	public String queryByAttributes() throws Exception {
-		if (model.getGoodsattributes() != null) {
-			logger.debug("godosattributes : "+model.getGoodsattributes());
-			String jsonString = ToJson.getGson().toJson(
-					solrUtil.queryBeans(model, model.getGoodsattributes()));
-			logger.debug("jsonString : "+jsonString);
-			jsonInputStream = new ByteArrayInputStream(
-					jsonString.getBytes("utf-8"));
-		}
-		return "done";
-	}
-
-	/**
-	 * 根据商品类型查询商品. List<GoodsVo>
-	 * 
-	 * @author 宋文光.
-	 * @return
-	 * @throws IOException
-	 * @throws SolrServerException
-	 * 
-	 */
-	public String queryByType() throws SolrServerException, IOException {
-		String jsonString = null;
-		if (model.getGoodstype3() != null) {
-			logger.debug("type3 : "+model.getGoodstype3());
-			jsonString = ToJson.getGson().toJson(
-					solrUtil.queryBeans(model, model.getGoodstype3()));
-			logger.debug("jsonString : "+jsonString);
-		} else if (model.getGoodstype2() != null) {
-			logger.debug("type2 : "+model.getGoodstype2());
-			jsonString = ToJson.getGson().toJson(
-					solrUtil.queryBeans(model, model.getGoodstype2()));
-			logger.debug("jsonString : "+jsonString);
-		}
-		jsonInputStream = new ByteArrayInputStream(
-				jsonString.getBytes("utf-8"));
-		return "done";
-	}
-
-	/**
-	 * 查询热点商品.
-	 * 
-	 * @author 宋文光.
-	 * 
-	 * @return
-	 * 
-	 * @throws IOException
-	 * @throws SolrServerException
-	 * 
-	 */
-	public String queryHot() throws SolrServerException, IOException {
-		model.setIshot(true);
-		String jsonString = ToJson.getGson().toJson(solrUtil.queryBeans(model));
-		logger.debug(jsonString);
-		jsonInputStream = new ByteArrayInputStream(jsonString.getBytes("utf-8"));
-		return "done";
-	}
-
-	/**
-	 * 根据关键字查询商品. 返回高亮集合. List<Object>.
-	 * 
-	 * @author 黄祥谦.
-	 * 
-	 * @return List<GoodsVo> 匹配goodsname,goodslable属性
-	 * 
-	 * @throws IOException
-	 * @throws SolrServerException
-	 * @throws InvocationTargetException
-	 * @throws IllegalArgumentException
-	 * @throws IllegalAccessException
-	 * @throws InstantiationException
-	 * 
-	 */
-	public String queryKeyWord() throws InstantiationException,
-			IllegalAccessException, IllegalArgumentException,
-			InvocationTargetException, SolrServerException, IOException {
-			logger.debug("keyWord : "+keyWord);
-			String jsonString = ToJson.getGson().toJson(
-					solrUtil.queryBean(keyWord));
-			logger.debug("jsonString : " + jsonString);
-			jsonInputStream = new ByteArrayInputStream(
-					jsonString.getBytes("utf-8"));
-			return "done";
-	}
-
-	/**
-	 * 
-	 * 通过查询条件删除索引.
-	 * 
-	 * @throws Exception
-	 * 
-	 */
-	// public void deleteByQuery() {
-	// solrService.deleteByQuery(query);
-	// }
 	
 	/**
-	 * 根据类别查询商品的属性. 赋值给Map<String,List<String>>
+	 * 
+	 * 通过查询条件更新索引.
 	 * 
 	 * @throws Exception
-	 */
-	public String getAttrbutes() throws Exception {
-			logger.debug("type2 : "+model.getGoodstype2());
-			logger.debug("type3 : "+model.getGoodstype3());
-			String jsonString = ToJson.getGson().toJson(
-					solrUtil.getAttributesByType(model));
-			logger.debug("jsonString : "+jsonString);
-			jsonInputStream = new ByteArrayInputStream(
-					jsonString.getBytes("utf-8"));
-		return "done";
-	}
-
-	/**
-	 * 获取所有类别.
 	 * 
-	 * @author 黄祥谦.
-	 * @return json数组，type1,type2,type3
-	 * 
-	 * @throws IOException
-	 * @throws SolrServerException
 	 */
-	public String getCategory() throws SolrServerException, IOException {
-		String jsonString = ToJson.getGson().toJson(solrUtil.getCategory());
-		logger.debug(jsonString);
-		jsonInputStream = new ByteArrayInputStream(jsonString.getBytes("utf-8"));
-		return "done";
+		public void updateGoodsAction() throws Exception {
+		
+		goodsServer.update(model);
+		GoodsVo goodsVo = new GoodsVo();
+		BeanUtils.copyProperties(goodsVo, model);
+		solrServer.addBeanIndex(goodsVo);
 	}
 }
--- src/main/webapp/js/goodslist.js
+++ src/main/webapp/js/goodslist.js
@@ -2,17 +2,17 @@
 $(function () {
 
     console.log(window.location.href.substr(window.location.href.lastIndexOf("?")+1));
-    tmptype3 = window.location.href.substr(window.location.href.lastIndexOf("?")+1);
+    tmptype = window.location.href.substr(window.location.href.lastIndexOf("?")+1);
 
-    var attributename = tmptype3.substr(tmptype3.lastIndexOf("=")+1);
-    var attributevalue = tmptype3.substr(0,tmptype3.lastIndexOf("="));
+    var attributename = tmptype.substr(tmptype.lastIndexOf("=")+1);
+    var attributevalue = tmptype.substr(0,tmptype.lastIndexOf("="));
     console.log(attributename);
     console.log(attributevalue);
 
     var goodslist_htmlobj = $.ajax({url: "queryKeyWord.txt", async: false});
 
     var attributes_htmlobj = $.ajax({url: "getAttribute.txt", async: false});
-    //var attributes_htmlobj = $.ajax({url: "http://localhost:8080/shoppingsolr2/goods_getAttrbutes.action?goodstype3="+tmptype3, async: false});
+//    var attributes_htmlobj = $.ajax({url: "http://localhost:8080/shoppingsolr2/goods_getAttrbutes.action?"+tmptype, async: false});
 
     var attributes = eval('(' + attributes_htmlobj.responseText + ")");
 
@@ -22,39 +22,42 @@ $(function () {
 
     postModel.push({ name : attributename, value : attributevalue});
 
-    $.each(categories, function (i, data) {
-
-        $.each(data.type2, function (i, data2) {
-
-            $(".main .container .class_selection_container .class_selection ul").append(
-                $("<li></li>").append(
-                    $("<a></a>").attr("href", "javascript:;").html(data2.name).on("click", function () {
-                        console.log(data2.name);
-                    })));
-
-            $.each(data2.type3, function (i, data3) {
-
-                $(".main .container .class_selection_container .class_selection ol").append(
-                    $("<li></li>").append(
-                        $("<a></a>").attr("href", "javascript:;").html(data3)).on("click", function () {
-                            console.log(data3);
-                            goodslist_htmlobj = $.ajax({url: "queryKeyWord2.txt", async: false});
-                            keyWordGoods = eval(goodslist_htmlobj.responseText);
-                            $(".main .container .goods_list .goods_show ul").empty();
-                            //console.log(keyWordGoods);
-                            $.each(keyWordGoods, function (i, data) {
-                                var goods = $("<li></li>").append(
-                                    $("<div class='good_img'></div>").append(
-                                        $("<img />").attr("src", "img/Q5423EFZ41_CGR0_N01.JPG"))).append(
-                                    $("<a class='name'></a>").attr("href", data.goodspic).html(data.goodsname)).append(
-                                    $("<span class='price'></span>").text(data.goodsprice)).append(
-                                    $("<em></em>").append($("<img />").attr("src", "img/icon_cart.png/")).html("1000"));
-                                $(".goods_show > ul").append(goods);
-                            });
-                        }));
-            });
-        });
-    });
+//    $.each(categories, function (i, data) {
+//
+//        $.each(data.type2, function (i, data2) {
+//
+//            $(".main .container .class_selection_container .class_selection ul").append(
+//                $("<li></li>").append(
+//                    $("<a></a>").attr("href", "javascript:;").html(data2.name).on("click", function () {
+//                        console.log(data2.name);
+//                    }
+//                    )
+//                )
+//            );
+//
+//            $.each(data2.type3, function (i, data3) {
+//
+//                $(".main .container .class_selection_container .class_selection ol").append(
+//                    $("<li></li>").append(
+//                        $("<a></a>").attr("href", "javascript:;").html(data3)).on("click", function () {
+//                            console.log(data3);
+//                            goodslist_htmlobj = $.ajax({url: "queryKeyWord2.txt", async: false});
+//                            keyWordGoods = eval(goodslist_htmlobj.responseText);
+//                            $(".main .container .goods_list .goods_show ul").empty();
+//                            //console.log(keyWordGoods);
+//                            $.each(keyWordGoods, function (i, data) {
+//                                var goods = $("<li></li>").append(
+//                                    $("<div class='good_img'></div>").append(
+//                                        $("<img />").attr("src", "img/Q5423EFZ41_CGR0_N01.JPG"))).append(
+//                                    $("<a class='name'></a>").attr("href", data.goodspic).html(data.goodsname)).append(
+//                                    $("<span class='price'></span>").text(data.goodsprice)).append(
+//                                    $("<em></em>").append($("<img />").attr("src", "img/icon_cart.png/")).html("1000"));
+//                                $(".goods_show > ul").append(goods);
+//                            });
+//                        }));
+//            });
+//        });
+//    });
 
     $.each(attributes, function (keyname, data) {
         var myul = $("<ul></ul>");
@@ -65,7 +68,6 @@ $(function () {
                 } else {
                     $(this).siblings().removeAttr("class");
                     $(this).attr("class", "press");
-
                 }
 
                 $(".classification_attr .press").parent().siblings().each(function (i, data2) {
@@ -73,6 +75,8 @@ $(function () {
                     postModel.push({ name : 'goodsAtributes', value : mydata});
                 });
                 //console.log(postModel);
+//                
+//                $.post('localhost:8080/shoppingsolr2/goods_queryByAttributes.action',postModel,function(data){
                 $.post('queryKeyWord.txt',postModel,function(data){
                     console.log(eval(data));
                     keyWordGoods = eval(data);
