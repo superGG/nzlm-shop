a03c942406330051718fa591ddf2ca4ef3454da6
LaLa <798555920@qq.com>
2015-10-01 21:43:51 +0800

    添加类别

---
+++ src/main/java/com/earl/shopping/daoImpl/CategoryDaoImpl.java
@@ -0,0 +1,14 @@
+package com.earl.shopping.daoImpl;
+
+import com.earl.shopping.dao.CategoryDao;
+import com.earl.solrj.query.pojo.CategoryPo;
+
+
+/**
+ * 
+ * 
+ */
+public class CategoryDaoImpl extends BaseDaoImpl<CategoryPo> implements CategoryDao{
+	// property constants
+	
+}
